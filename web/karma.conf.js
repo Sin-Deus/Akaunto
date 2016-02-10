6e4ce1697bc2c14d460f8c65cb5d7cec8485a3e1
Sin-Deus <Sin-Deus@users.noreply.github.com>
2016-02-10 14:40:09 +0100

    Fix the Karma configuration to correctly serve files.

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -3,9 +3,10 @@
 module.exports = function (config) {
     config.set({
         frameworks: ['jspm', 'jasmine'],
+        files: ['node_modules/jasmine-async-sugar/jasmine-async-sugar.js'],
         jspm: {
-            loadFiles: ['test/scripts/**/*.js'],
-            serveFiles: ['src/scripts/**/*.js']
+            loadFiles: ['test/scripts/**/*.js', 'src/scripts/**/*.js'],
+            serveFiles: ['src/scripts/*.js', 'src/scripts/**/*.js']
         },
         browsers: ['PhantomJS']
     });
