b8475fd7d3999c3319816e671e9163ad7a529489
Sin-Deus <Sin-Deus@users.noreply.github.com>
2016-02-10 15:12:26 +0100

    Add tests for the user service.

---
+++ web/test/scripts/services/user.spec.js
@@ -0,0 +1,29 @@
+import { module, inject } from 'angular-mocks';
+
+describe('UserService', () => {
+    let userService;
+    let $httpBackend;
+    let baseConstants;
+
+    beforeEach(module('akaunto'));
+
+    beforeEach(inject(function (_$httpBackend_, _baseConstants_, _userService_) {
+        $httpBackend = _$httpBackend_;
+        baseConstants = _baseConstants_;
+        userService = _userService_;
+    }));
+
+    describe('getMe', () => {
+        it('should be defined', () => {
+            expect(userService.getMe).toBeDefined();
+        });
+
+        itAsync('should call the correct URL', () => {
+            $httpBackend.expectGET(baseConstants.apiBaseURL + 'users/me').respond({ username: 'Test McMuffin' });
+            let promise = userService.getMe();
+            $httpBackend.flush();
+
+            return promise.then(user => expect(user).toEqual({ username: 'Test McMuffin' }));
+        });
+    });
+});
