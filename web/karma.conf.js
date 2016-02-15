ce76b94aa643fa70ef5d1c2069e68fb73f893001
Sin-Deus <Sin-Deus@users.noreply.github.com>
2016-02-15 09:30:58 +0100

    Add code coverage.

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -8,6 +8,23 @@ module.exports = function (config) {
             loadFiles: ['test/scripts/**/*.js', 'src/scripts/**/*.js'],
             serveFiles: ['src/scripts/*.js', 'src/scripts/**/*.js']
         },
-        browsers: ['PhantomJS']
+        browsers: ['PhantomJS'],
+        reporters: ['coverage'],
+        preprocessors: {
+            'src/scripts/**/*.js': ['babel', 'coverage']
+        },
+        babelPreprocessor: {
+            options: {
+                presets: ['es2015'],
+                sourceMap: 'inline'
+            }
+        },
+        coverageReporter: {
+            dir: 'test-reports/coverage/',
+            reporters: [
+                {type: 'html'},
+                {type: 'text-summary'}
+            ]
+        }
     });
 };
