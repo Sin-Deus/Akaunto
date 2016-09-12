768cf35c4ad3c8e5e4df887cd0b0233843dfaa6a
Sin-Deus <Sin-Deus@users.noreply.github.com>
2016-09-12 11:45:41 +0200

    Send status code instead of throwing error in the authentication process.

--- server/app/routes/authentication.js
+++ server/app/routes/authentication.js
@@ -11,13 +11,13 @@ router.route('/').post((req, res) => {
         if (err) { throw err; }
 
         if (!user) {
-            res.send(401);
+            res.sendStatus(401);
         } else {
             user.comparePassword(req.body.password, (err, isMatch) => {
-                if (err) { throw err; }
-
-                if (!isMatch) {
-                    res.send(400);
+                if (err) {
+                    res.status(400).send(err);
+                } else if (!isMatch) {
+                    res.sendStatus(400);
                 } else {
                     let token = jwt.sign(user, config.secret, {
                         expiresIn: 3600 // expires in 1 hour
