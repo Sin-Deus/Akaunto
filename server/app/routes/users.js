c1f9a5d8b0233d486565d5748519d3e5053c756f
Sin-Deus <Sin-Deus@users.noreply.github.com>
2016-09-13 10:45:36 +0200

    Add some user routes.

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -7,5 +7,37 @@ module.exports = router => {
         User.find({}, (err, users) => res.json(users));
     });
 
+    router.route('/').post((req, res) => {
+        let user = new User(req.body);
+
+        user.save(err => {
+            if (err) {
+                res.sendStatus(400);
+            } else {
+                res.json(user);
+            }
+        })
+    });
+
+    router.route('/:id').get((req, res) => {
+        User.findOne({ _id: req.params.id }, (err, user) => {
+            if (err || !user) {
+                res.sendStatus(404);
+            } else {
+                res.json(user);
+            }
+        });
+    });
+
+    router.route('/:id').put((req, res) => {
+        User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, user) => {
+            if (err || !user) {
+                res.sendStatus(400);
+            } else {
+                res.json(user);
+            }
+        });
+    });
+
     return router;
 };
