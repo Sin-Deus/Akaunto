698f5c64b5f61dad0f98c66011e4f0d362b8adda
Sin-Deus <Sin-Deus@users.noreply.github.com>
2016-10-20 15:28:56 +0200

    Add tests for authentication.

--- server/test/authentication.spec.js
+++ server/test/authentication.spec.js
@@ -1,31 +1,64 @@
-const request = require('supertest');
-const Mongoose = require('mongoose').Mongoose;
-const mongoose = new Mongoose();
-const mockgoose = require('mockgoose');
+process.env.NODE_ENV = 'test';
 
-describe('Loading express', function () {
-    var server;
-    
-    before(function(done) {
-        mockgoose(mongoose).then(function() {
-            mongoose.connect('mongodb:27017/akaunto_test', function (err) {
-                done(err);
-            });
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let User = require('../app/models/User');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Authentication', () => {
+    before(() => {
+        let user = new User({
+            email: 'test@test.com',
+            password: 'p@ssw0rd'
         });
+        return user.save();
     });
 
-    beforeEach(() => {
-        server = require('../server');
-    });
+    after(() => User.remove({}).exec());
 
-    afterEach(() => {
-        server.close();
-    });
+    describe('/POST authenticate', () => {
+        it('should respond to the request', done => {
+            chai.request(server)
+                .post('/authenticate')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
 
-    it('should respond to /authenticate', function (done) {
-        console.log('start');
-        request(server)
-            .post('/authenticate', {})
-            .expect(403, done);
-    });
+        it('should not find any user with a wrong e-mail', done => {
+            chai.request(server)
+                .post('/authenticate')
+                .send({ email: 'unusedemail@gmail.com', password: 'password' })
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+
+        it('should find a user, but fail to authenticate with a wrong password', done => {
+            chai.request(server)
+                .post('/authenticate')
+                .send({ email: 'test@test.com', password: 'password' })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+
+        it('should find a user, and return a valid token with the correct password', done => {
+            chai.request(server)
+                .post('/authenticate')
+                .send({ email: 'test@test.com', password: 'p@ssw0rd' })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('token');
+                    done();
+                });
+        });
+    })
 });
